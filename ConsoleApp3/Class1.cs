d5b97419e275d0e64d193b6e189a1fdae500fe8b
rgfle <rgfle@DESKTOP-M14RMBO>
2024-06-01 01:26:12 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public abstract class Растение
+    {
+        public string Сорт { get; set; }
+    }
+
+    public class Яблоня : Растение
+    {
+    }
+
+    public class Смородина : Растение
+    {
+    }
+
+    public class Сад
+    {
+        public string Садовник { get; set; }
+        public List<Растение> Растения { get; set; }
+
+        public Сад()
+        {
+            Растения = new List<Растение>();
+        }
+
+        public void Посадить(Растение растение)
+        {
+            Растения.Add(растение);
+            Console.WriteLine($"Садовник {Садовник} посадил дерево {растение.Сорт}");
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Сад сад = new Сад { Садовник = "Иван" };
+            Яблоня яблоня = new Яблоня { Сорт = "Антоновка" };
+            Смородина смородина = new Смородина { Сорт = "Черная" };
+
+            сад.Посадить(яблоня);
+            сад.Посадить(смородина);
+        }
+    }
+}
